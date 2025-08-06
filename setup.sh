5ea8a049965f38febbc4479c0ff7ded235553bb1
TheTorch <miner2000@outlook.de>
2025-08-06 22:13:42 +0200

    probably working test version still have some holes in security

--- setup.sh
+++ setup.sh
@@ -3,16 +3,15 @@
 podman build docker -t flutter
 # initialize the container and open an port for ssh
 mkdir ~/flutter
-xhost + 
+xhost +
 # podman run -d -i --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 --security-opt label=type:container_runtime_t -v /dev/bus/usb:/dev/bus/usb --name FlutterDevContainer --replace -v ~/flutter:/flutter:z -p [::1]:20202:22 localhost/flutter
 podman run -d --privileged \
--v /etc/localtime:/etc/localtime:ro \
--v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
---security-opt label=disable \
--v ~/flutter:/flutter:z \
---net=host \
---replace \
---name FlutterDevContainer -ti localhost/flutter
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 --security-opt label=disable \
+  -v ~/flutter:/flutter:z \
+  --net=host \
+  --replace \
+  --name FlutterDevContainer -ti localhost/flutter
 #make the container reachable with ssh
 ## set a temporary Password to log in from ssh and launch ssh
 echo "please set the root password for the container"
@@ -28,11 +27,11 @@ ssh-keygen -t ed25519
 ssh-copy-id -i ~/.ssh/id_ed25519.pub -p 20202 root@::1
 
 ##Preconfigure ssh
-echo "Host FlutterDevContaier" >> $HOME/.ssh/config
-echo "    HostName ::1" >> $HOME/.ssh/config
-echo "    User root" >> $HOME/.ssh/config
-echo "    Port 20202" >> $HOME/.ssh/config
+echo "Host FlutterDevContaier" >>$HOME/.ssh/config
+echo "    HostName ::1" >>$HOME/.ssh/config
+echo "    User root" >>$HOME/.ssh/config
+echo "    Port 20202" >>$HOME/.ssh/config
 
-## install vsc ssh extension 
+## install vsc ssh extension
 flatpak install com.vscodium.codium
 flatpak run com.vscodium.codium --install-extension 3timeslazy.vscodium-devpodcontainers
