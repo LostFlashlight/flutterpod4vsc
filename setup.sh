4b0921d092624ae915211deb054fccc784c96707
TheTorch <miner2000@outlook.de>
2025-08-06 02:21:06 +0200

    trying to get gui to work

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 # build the image out of the Docker file
-podman build docker -t flutter
+#podman build docker -t flutter
 # initialize the container and open an port for ssh
 mkdir ~/flutter
-podman run -d -i --privileged -v /dev/bus/usb:/dev/bus/usb --name FlutterDevContainer --replace -v ~/flutter:/flutter:z -p [::1]:20202:22 localhost/flutter
+xhost + 
+# podman run -d -i --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 --security-opt label=type:container_runtime_t -v /dev/bus/usb:/dev/bus/usb --name FlutterDevContainer --replace -v ~/flutter:/flutter:z -p [::1]:20202:22 localhost/flutter
+podman run -d --privileged \
+-v /etc/localtime:/etc/localtime:ro \
+-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+--security-opt label=type:container_runtime_t \
+-v ~/flutter:/flutter:z \
+-p [::1]:20202:22 \
+--replace \
+--name FlutterDevContainer -ti localhost/flutter
+
 #make the container reachable with ssh
 ## set a temporary Password to log in from ssh and launch ssh
 echo "please set the root password for the container"
